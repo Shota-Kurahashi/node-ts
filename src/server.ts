ae0f069431d69bc68e5f2cacaee6cddebb4df4c3
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-07-09 15:05:49 +0900

    init

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
 
